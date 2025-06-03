8099fc72ccf514f2e0f569e0dfcbed54770451c3
Otter963 <averagetoast963@gmail.com>
2025-06-03 21:13:37 +0200

    Created header

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+Reference:
+Title: How to make a portfolio website using HTML CSS JS | Complete responsive website design
+Author:GreatStack
+Date: 2022, July 14
+Availability: https://youtu.be/0YFrGy_mzjY?si=Ov4rLueFSznqQ2Tg
+*/
