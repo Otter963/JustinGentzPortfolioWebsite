ee97af9601efe5e5f1f73cc8ec3045d48ea4cda7
Otter963 <averagetoast963@gmail.com>
2025-06-03 21:57:15 +0200

    Added the about section

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ Author:GreatStack
 Date: 2022, July 14
 Availability: https://youtu.be/0YFrGy_mzjY?si=Ov4rLueFSznqQ2Tg
 */
+
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+function openTab(tabName){
+    for (tabLink of tabLinks){
+       tabLink.classList.remove("active-link");
+    }
+        for (tabContent of tabContents){
+       tabContent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
+}
